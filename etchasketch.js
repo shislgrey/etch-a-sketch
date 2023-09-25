a183ea6f9b37f73b18aba676b266d2035361bcbd
Grey <shisler.grace@gmail.com>
2023-09-25 00:19:03 -0700

    more todos
    Co-authored-by: Pavel <pkirkovsky@users.noreply.github.com>

--- etchasketch.js
+++ etchasketch.js
@@ -1,6 +1,15 @@
+
+//done: create a grid of 16x16 squares
+//TODO: when mouse hovers over square, it changes color
+//TODO: add a button to clear the grid and prompt for new grid size
+
+//TODO: add a button to change the color of the squares
+//TODO: add a button to change the color of the squares to random colors
+//TODO: add a progressively darker shade of gray for each pass over the square
+
 let size = 16
 
-function etchASketch() {
+function generateGrid() {
     for (let column = 0; column < size; column++) {
         var line = document.createElement('div')
         line.classList.add('line')
@@ -8,19 +17,20 @@ function etchASketch() {
             let square = document.createElement('div')
             square.classList.add('square')
             line.appendChild(square)
+            square.addEventListener('mouseover', () => { updateColor(); })
         }
         document.querySelector(".main").appendChild(line)
     }
 }
-
-//TODO: figure out why doodle isn't working how I think it's supposed to. 
+//TODO: figure out why changeColor isn't working how I think it's supposed to. 
 // classList? maybe I have to add a whole nother div?
 // 2do'ing this so I don't forget what I wanted to unpick tomorrow
 
-function doodle() {
+function updateColor() {
     document.querySelector('.square').classList.add('hovered')
 }
 
-etchASketch()
+generateGrid()
 
-document.querySelector('.square').addEventListener('mouseover', () => { doodle(); })
+// var squares = document.querySelectorAll('.square')
+// squares.forEach((square) => { addEventListener('mouseover', () => { updateColor(); }); })
